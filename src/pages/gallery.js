ffaa90a29df2c8a05085a6a0bc52d901b9d7efb6
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2021-09-23 14:49:11 -0700

    use mui imagelist and imagelistitem

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { GatsbyImage } from "gatsby-plugin-image";
 import Img from "gatsby-image";
-import { ImageListItem, ImageList } from "gatsby-material-ui-components";
+import { ImageList, ImageListItem } from "@mui/material";
 
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
@@ -20,16 +20,16 @@ const GalleryPage = (props) => {
   const tokens = props.data.allNftAssets.edges;
 
   return (
-    <div>
+    <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
       {tokens.map((token, i) => {
         const tokenData = token.node;
         return (
-          <div key={i}>
+          <ImageListItem key={i}>
             <img src={tokenData.image_thumbnail_url} />
-          </div>
+          </ImageListItem>
         );
       })}
-    </div>
+    </ImageList>
   );
 };
 
