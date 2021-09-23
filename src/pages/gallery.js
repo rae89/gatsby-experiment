9c3d600bbf70eb60a8867b058887251e6d166e30
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2021-09-23 15:45:23 -0700

    center imagelist

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -6,30 +6,32 @@ import { ImageList, ImageListItem } from "@mui/material";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
 
-// const GalleryPage = () => (
-//   <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
-//     {itemData.map((item) => (
-//       <ImageListItem key={item.img}>
-//         <img src={tokenData.image_thumbnail_url} />
-//       </ImageListItem>
-//     ))}
-//   </ImageList>
-// );
-
 const GalleryPage = (props) => {
   const tokens = props.data.allNftAssets.edges;
 
   return (
-    <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
-      {tokens.map((token, i) => {
-        const tokenData = token.node;
-        return (
-          <ImageListItem key={i}>
-            <img src={tokenData.image_thumbnail_url} />
-          </ImageListItem>
-        );
-      })}
-    </ImageList>
+    <Layout>
+      <ImageList
+        sx={{
+          ml: "auto",
+          mr: "auto",
+          maxWidth: 600,
+          height: 1200,
+          overflow: "hidden",
+        }}
+        cols={3}
+        rowHeight={164}
+      >
+        {tokens.map((token, i) => {
+          const tokenData = token.node;
+          return (
+            <ImageListItem key={i}>
+              <img src={tokenData.image_thumbnail_url} />
+            </ImageListItem>
+          );
+        })}
+      </ImageList>
+    </Layout>
   );
 };
 
