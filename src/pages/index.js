a540a3245bf4e752487aad2b0758feb9e7fba0f3
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2021-09-28 19:38:02 -0700

    add containers

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,34 @@ import Splash from "../components/splash";
 import Layout from "../components/layout";
 import styled from "styled-components";
 import { Button, Link } from "gatsby-theme-material-ui";
+import { Box, Container } from "@material-ui/core";
+import { navigate } from "gatsby-link";
 
 export default function IndexPage() {
   return (
     <Layout>
-      <Button>
-        <Link to="/gallery">Enter</Link>
-      </Button>
-      <Splash></Splash>
+      <Container>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          minHeight="40vh"
+        >
+          <Button variant="contained" onClick={() => navigate("/gallery")}>
+            Enter
+          </Button>
+        </Box>
+      </Container>
+      <Container>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          minHeight="100vh"
+        >
+          <Splash></Splash>
+        </Box>
+      </Container>
     </Layout>
   );
 }
